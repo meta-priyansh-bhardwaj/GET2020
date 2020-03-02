9948ea61a8be0aa19de8b179cfb4e109d4c5d1c5
Priyansh Bhardwaj <priyansh.bhardwaj@metacube.com>
2020-03-02 20:42:50 +0530

    Animation added

--- UI/Assignment2/scripts/scripts.js
+++ UI/Assignment2/scripts/scripts.js
@@ -8,4 +8,26 @@ $(document).ready(function(){
         }, 800, function(){});
       }
     });
+
+    $('.registrationForm').hide();
+    $('#pricing').hide();
+
+    $(".registrationFormOption").click(function(event){
+      event.preventDefault();
+      $('.registrationForm').hide();
+      $('#pricing').hide();
+      let index = ($(this).index()-1)/2;
+      console.log(index)
+      if(index < 3){
+        $('.registrationForm').eq(index).show(500);
+        $('html, body').animate({
+          scrollTop: $(".registrationForm").eq(index).offset().top-60
+        }, 800, function(){});
+      } else {
+        $('#pricing').show(500);
+        $('html, body').animate({
+          scrollTop: $("#pricing").offset().top-60
+        }, 800, function(){});
+      }
+    });
   });
